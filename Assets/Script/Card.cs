0aed8b7f14d07c7e9b016234e747db204bae30ff
GGWAkGGWAK <dltlgus15@naver.com>
2025-10-20 18:52:38 +0900

    Initial commit

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int cardShape;
+    public int cardNumber;
+    public Sprite cardImage;
+
+    private Rigidbody2D rigid;
+    
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
